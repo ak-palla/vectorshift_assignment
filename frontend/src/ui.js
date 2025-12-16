af67f2c95c066ab7b173e98d2856513e111d65ca
Akhil Palla <akhilpalla247@gmail.com>
2025-12-16 18:29:30 +0530

    Build error fixed - ready for deployment

--- frontend/src/ui.js
+++ frontend/src/ui.js
@@ -96,7 +96,7 @@ export const PipelineUI = () => {
         addNode(newNode);
       }
     },
-    [reactFlowInstance]
+    [reactFlowInstance, getNodeID, addNode]
   );
 
   const onDragOver = useCallback((event) => {
